8f42d88bf6506163e0e7164f13e493760dae1a5e
rythroja <jared@mercenarytricking.com>
2023-12-18 13:22:37 -0800

    use for loop and an new row array to capture user input and append it to table in page

--- script.js
+++ script.js
@@ -4,14 +4,34 @@ const inputAuthor = document.getElementById('author');
 const inputStatus = document.getElementById('read');
 const inputRating = document.getElementById('rating')
     const sliderOutput = document.getElementById('rating-input');
-    sliderOutput.addEventListener('input', ()=>{sliderOutput.innerText = inputRating.value})
+    inputRating.addEventListener('input', ()=>{sliderOutput.innerText = inputRating.value
+    })
+const displayTable = document.getElementById('display-table');
     
-
 
 submitButton.addEventListener("click",()=>{
     addRow();
 });
 
+
+
 function addRow() {
-    console.log("i work");
+    let nextRow = document.createElement('tr');
+
+    let row = [inputTitle.value, inputAuthor.value,
+        inputStatus.value, inputRating.value]
+
+        for(let i = 0; i < row.length; i+=1){
+            let td = document.createElement('td');
+                td.innerText = row[i];
+            nextRow.appendChild(td);
+        }
+    let del = document.createElement('td');
+        del.innerText = 'delete';
+        del.className = 'delete';
+        nextRow.appendChild(del);
+    
+    displayTable.appendChild(nextRow);
+
+    
 }
