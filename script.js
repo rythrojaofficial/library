7e49d9aeffdab3ee14e79e24f8c771d78628786d
rythroja <jared@mercenarytricking.com>
2023-12-20 11:28:39 -0800

    add rating functionality using classlist.add to the td in addRow

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let count = 0;
+
+
+
 
 
 const submitButton = document.getElementById('submit-button');
+    submitButton.addEventListener("click",()=>{
+                    addRow();
+    });
 const inputTitle = document.getElementById('book');
 const inputAuthor = document.getElementById('author');
 const inputStatus = document.getElementById('read');
@@ -11,12 +16,6 @@ const inputRating = document.getElementById('rating')
     })
 const displayTable = document.getElementById('display-table');
 
-    
-
-submitButton.addEventListener("click",()=>{
-    addRow(inputTitle.innerText);
-    count += 1;
-});
 
 const library=[];
 // constructor
@@ -31,31 +30,75 @@ function Book(title, author, status, rating){
 
 
 
-function addRow(theTitle) {
-    // let new Book(theTitle, )
+function addRow() {
+    let currentBook = new Book(inputTitle.value, inputAuthor.value,
+                            inputStatus.value, inputRating.value);
+    library.push(currentBook);
 
 
     let nextRow = document.createElement('tr');
+    let del = document.createElement('td');
+    let row = [library.length, inputTitle.value, inputAuthor.value,
+        inputStatus.value, sliderOutput.innerText, del]
 
-    let row = [count, inputTitle.value, inputAuthor.value,
-        inputStatus.value, inputRating.value]
-
+        // read status 
         for(let i = 0; i < row.length; i+=1){
             let td = document.createElement('td');
                 td.innerText = row[i];
+                if(td.innerText === 'read'){
+                    td.classList.add('read');
+                }else if(td.innerText === 'not read'){
+                    td.classList.add('notRead');
+                } 
+            if (i===3){
+                td.classList.add('status');
+                td.addEventListener('click', ()=>{
+                    if (td.innerText === 'read'){
+                        td.innerText = 'not read';
+                        td.classList.remove('read');
+                        td.classList.add('notRead');
+                    }else if (td.innerText === 'not read'){
+                        td.innerText = 'read';
+                        td.classList.remove('notRead');
+                        td.classList.add('read');
+
+                    }else return
+                })
+            }
+            //rating row
+            if(i==4){
+                let theRating = Number(td.innerText);
+                // console.log(`theRating is ${theRating}, which is a ` + typeof(theRating));
+
+                
+                if(theRating > 0 && theRating < 2.5){
+                    td.classList.add('rated-poorly');
+                }else if(theRating >= 2.5 && theRating < 4){
+                    td.classList.add('rated-okay')
+                }else if(theRating >= 4 && theRating <= 5){
+                    td.classList.add('rated-well');
+                }else td.classList.add('notRated');
+                
+               
+            }
+            // delete row
+            if (i==5){
+                td.innerText = 'delete';
+                td.className = 'delete';
+                td.addEventListener('click', ()=>{
+                    let text;
+                    if(confirm(`You are about to delete entry ${del.parentElement.firstChild.innerText}`)){
+                        displayTable.removeChild(nextRow);
+                        delete library[del.parentElement.firstChild.innerText-1]
+                        
+                    }
+                    else return;
+                    
+                })
+            }
+            
             nextRow.appendChild(td);
         }
-    let del = document.createElement('td');
-        del.innerText = 'delete';
-        del.className = 'delete';
-        del.addEventListener('click', ()=>{
-            let text;
-            if(confirm(`You are about to delete a row`)){
-                displayTable.removeChild(nextRow)
-            }
-            else return;
-            
-        });
         nextRow.appendChild(del);
     
     displayTable.appendChild(nextRow);
